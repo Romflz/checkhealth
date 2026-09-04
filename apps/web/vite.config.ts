52c4a804ad8f0ad25bb41cce6eeff1e1c2d7a299
Roman <electronicmusicplease@gmail.com>
2026-09-04 18:50:30 +0200

    chore: switch from vue to react

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,16 +1,7 @@
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  }
+  plugins: [react()],
 })
