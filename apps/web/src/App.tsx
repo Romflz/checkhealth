b93a65b0ed88aebfd8ca0b6e61f6b08b4b826e93
Roman <electronicmusicplease@gmail.com>
2026-09-04 19:10:24 +0200

    fix: web naming, fix: web script, feat: vite proxy, feat: api health call

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -6,6 +6,11 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    fetch('/api/health').then((res) => res.json().then((data) => {
+      console.log('data', data)
+    }))
+  }, [])
 
   return (
     <>
